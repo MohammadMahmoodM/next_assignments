474be23182bc92509cb3f5f4fe0a90d547e409e0
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-11-24 14:04:41 +0400

    almost completed, assignment two and making it ready for online link

--- assignment-two/src/app/page.tsx
+++ assignment-two/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from '../compnenet/header'
-import Course from '../compnenet/course'
-import {Campus} from '../compnenet/campus'
-import {Facilities} from '../compnenet/facilities'
-import {Testimonials} from '../compnenet/testimonials'
-import {Call_To_Action} from '../compnenet/call_to_action'
-import {Footer} from '../compnenet/footer'
+import Header from '../componenet/header'
+import Course from '../componenet/course'
+import {Campus} from '../componenet/campus'
+import {Facilities} from '../componenet/facilities'
+import {Testimonials} from '../componenet/testimonials'
+import {Call_To_Action} from '../componenet/call_to_action'
+import {Footer} from '../componenet/footer'
 
 export default function Home() {
   return (
