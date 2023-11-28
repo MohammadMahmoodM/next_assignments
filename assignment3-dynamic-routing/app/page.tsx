397637462a5f5496f0bd5915c5f036700df7d0fb
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-11-28 22:18:27 +0400

    user page added, now we will try to bring users on here

--- assignment3-dynamic-routing/app/page.tsx
+++ assignment3-dynamic-routing/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       Home Page
+      <br />
+      <br />
+      <Link href="/users">Users</Link>
     </main>
   )
 }
