904de69e26dc6f2920bf569d8b8f0be8a3fe0009
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-12-26 23:18:32 +0400

    making a bog app, making a responsive website which will have filter and mapping and tailwind and typescript just started byuilding, basic structure mean just hero section

---
+++ assignment8_make_blog_assignment/components/CustomButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+import {CustomButtonProps} from '../types/index'
+
+const CustomButton = (props:CustomButtonProps) => {
+    return (
+        <button 
+        disabled={false}
+        type={"button"}
+        className={props.containerStyles}
+        onClick={props.handleClick}
+        >
+            <span className={`flex-1`}>
+            {props.title}
+            </span>
+        </button>
+    )
+}
+
+export default CustomButton
---
+++ assignment8_make_blog_assignment/components/Hero.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Image from "next/image";
+import { CustomButton } from "@/components";
+
+const Hero = () => {
+
+  const handleScroll = () => {
+    alert("Button Clicked")
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">FIne, Book, and Rent a Car</h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with out effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue
+                text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
