013e52082a6603b50e6ba79a6b9e5119230228c7
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-12-27 23:38:28 +0400

    footer completed right now, previous hero and navbar was also completed

--- assignment8_make_blog_assignment/components/Footer.tsx
+++ assignment8_make_blog_assignment/components/Footer.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      Footer
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
 
-export default Footer
+          <p className="text-base text-gray-700">
+            Carhub 2024 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((footerLink) => {
+            return (
+              <div key={footerLink.title} className="footer_link">
+                <h3 className="font-bold">{footerLink.title}</h3>
+
+                {footerLink.links.map((LinkItem) => {
+                  return (
+                    <div>
+                      <Link href={LinkItem.url} className="text-gray-500">{LinkItem.title}</Link>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
