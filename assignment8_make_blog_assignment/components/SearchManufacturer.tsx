306ab205d665b2feeb9d515b782fb0496edee443
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-12-29 12:14:16 +0400

    filtering of manufacturer name is done. and in search panel you can see desired manufature who's name match with your query

--- assignment8_make_blog_assignment/components/SearchManufacturer.tsx
+++ assignment8_make_blog_assignment/components/SearchManufacturer.tsx
@@ -1,37 +1,74 @@
-'use client'
+"use client";
 
-import React, {useState} from 'react'
-import Image from 'next/image'
+import React, { Fragment, useState } from "react";
+import Image from "next/image";
+import { manufacturers } from "@/constants";
 
-import { Combobox, Transition } from '@headlessui/react'
+import { Combobox, Transition } from "@headlessui/react";
 
-const SearchManufacturer = (props:any) => {
-   
-    return (
-        <div className="search-manufacturer">
-            <Combobox>
-                <div className="relative w-full">
-                    <Combobox.Button className="absolute top-[14px]" style={{border: "1px solid black"}}>
-                        <Image 
-                        src="/car-logo.svg"
-                        width={20}
-                        height={20}
-                        className="ml-4"
-                        alt="car logo"
-                        />
-                    </Combobox.Button>
+const SearchManufacturer = (props: any) => {
+  const [query, setQuery] = useState("");
 
-                    <Combobox.Input 
-                        className="search-manufacturer__input"
-                        placeholder="Carolla"
-                       displayValue={(manufacturer:string)=>manufacturer}
-                    />
-                        
+  const filteredManufacturers = query === ""
+    ? manufacturers
+    : manufacturers.filter((item) => {
+        return (item
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.toLowerCase().replace(/\s+/g, ""))
+        )
+        });
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="car logo"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Carolla"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+                {filteredManufacturers.length === 0 && query !== "" ? (
+                    <Combobox.Option value={query}>
+                        {query}
+                    </Combobox.Option>
                     
-                </div>
-            </Combobox>
+                ) : (
+                   filteredManufacturers.map((item) => {
+                       return (
+                        <Combobox.Option value={item}>
+                            {item}
+                        </Combobox.Option>
+                       )
+                   })
+                )
+                }
+            </Combobox.Options>
+          </Transition>
         </div>
-    )
-}
+      </Combobox>
+    </div>
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
