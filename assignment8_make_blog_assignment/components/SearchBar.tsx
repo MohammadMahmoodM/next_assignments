afb5fde35b1a23d9ed327f1a392e6c2428c6c4b2
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2024-01-04 00:21:25 +0400

    search filter for car and manufacturer is working

--- assignment8_make_blog_assignment/components/SearchBar.tsx
+++ assignment8_make_blog_assignment/components/SearchBar.tsx
@@ -1,31 +1,80 @@
-'use client'
-import {useState} from 'react'
-import {SearchManufacturer} from '@/components'
-import {SearchButton} from '@/components'
-
+"use client";
+import { useState } from "react";
+import { SearchManufacturer } from "@/components";
+import { SearchButton } from "@/components";
+import Image from "next/image";
+import {useRouter} from 'next/navigation';
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const [carModel, setCarModel] = useState("");
 
-    const [manufacturer, setManufacturer] =  useState('')
+  const router = useRouter()
 
-    const handleSearch = () => {
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
 
+    if (manufacturer === "" && carModel === "") {
+      return alert("Please Enter Company and Car Model");
     }
 
-    return (
-        <form className="searchbar" onSubmit={handleSearch}>
-            <div className="searchbar__item">
-               <SearchManufacturer 
-               manufacturer = {manufacturer}
-               setManufacturer = {setManufacturer}
-               />
+    updateSearchparams(carModel.toLowerCase(), manufacturer.toLowerCase())
+  };
 
-                <SearchButton otherClasses="sm:hidden" />
-            </div>
+  const updateSearchparams = (carModel: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
 
-            
-        </form>
-    )
-}
+    if (carModel) {
+      searchParams.set("Car Model", carModel);
+    } else {
+      searchParams.delete("Car model");
+    }
 
-export default SearchBar
+    if (manufacturer) {
+      searchParams.set("Manufacturer", manufacturer);
+    } else {
+      searchParams.delete("Manufacturer");
+    }
+    const newPathname = `${window.location.pathname}${searchParams.toString()}`
+    router.push(newPathname)
+    
+  };
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model icon"
+        />
+        <input
+          type="text"
+          name="model"
+          value={carModel}
+          onChange={(e) => {
+            setCarModel(e.target.value);
+          }}
+          placeholder="Carolla"
+          className="searchbar_input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
+  );
+};
+
+export default SearchBar;
